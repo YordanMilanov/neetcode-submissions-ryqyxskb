561a241b5f3e46bc000f8bc94bf71a7ec7e48266
Yordan Milanov <106768384+YordanMilanov@users.noreply.github.com>
2026-04-12 00:22:29 +0300

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cs
@@ -0,0 +1,57 @@
+public class Solution {
+    public string MinWindow(string s, string t) {
+        if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t))
+            return "";
+
+        var need = new Dictionary<char, int>();
+
+        foreach (char c in t)
+        {
+            need[c] = need.GetValueOrDefault(c, 0) + 1;
+        }
+
+        int left = 0;
+        int remaining = t.Length;
+
+        int minLen = int.MaxValue;
+        int start = 0;
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            char c = s[right];
+
+            if (need.ContainsKey(c))
+            {
+                need[c]--;
+
+                if (need[c] >= 0)
+                    remaining--;
+            }
+
+            while (remaining == 0)
+            {
+                if (right - left + 1 < minLen)
+                {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s[left];
+
+                if (need.ContainsKey(leftChar))
+                {
+                    need[leftChar]++;
+
+                    if (need[leftChar] > 0)
+                        remaining++;
+                }
+
+                left++;
+            }
+        }
+
+        return minLen == int.MaxValue 
+            ? "" 
+            : s.Substring(start, minLen);
+    }
+}
